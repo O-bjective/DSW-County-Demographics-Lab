c1c3aa0ae2aed592cb2a3e3864e54785088561f3
O-bjective <jarydchandos.5@gmail.com>
2018-10-24 12:47:19 -0700

    jaryd

--- countyDemographicsUnfinished.py
+++ countyDemographicsUnfinished.py
@@ -11,29 +11,42 @@ def main():
 
 def alphabetically_first_county(counties):
     """Return the county with the name that comes first alphabetically."""
-
+    first = counties[0]["County"]
+    for county in counties :
+        if county["County"] < first:
+            first = county["County"]
+    return first
 
 def county_most_under_18(counties):
     """Return the name and state of a county ("<county name>, <state>") with the highest percent of under 18 year olds."""
+    first = counties[0]["Age"]["Percent Under 18 Years"]
+    state = counties[0]["State"]
+    for county in counties :
+        if county["State"] < state:
+            state = county["State"]
+        if county["Age"]["Percent Under 18 Years"] < first:
+            first = county["Age"]["Percent Under 18 Years"]
+    return first
 
-    
+
 def percent_most_under_18(counties):
     """Return the highest percent of under 18 year olds."""
+    first =
+    percent =
 
-    
 def most_under_18(counties):
     """Return a list with the name and state of a county ("<county name>, <state>") and the percent of under 18 year olds for a county with the highest percent of under 18 year olds."""
 
-    
+
 def state_with_most_counties(counties):
     """Return a state that has the most counties."""
     #Make a dictionary that has a key for each state and the values keep track of the number of counties in each state
-    
+
     #Find the state in the dictionary with the most counties
-    
+
     #Return the state with the most counties
-    
-    
+
+
 def your_interesting_demographic_function(counties):
     """Compute and return an interesting fact using the demographic data about the counties in the US."""
 
