e6ebd03d8a2998869b37ca57acf4d6ea8644ff69
O-bjective <jarydchandos.5@gmail.com>
2018-10-29 10:33:17 -0700

    jaryd

--- countyDemographicsUnfinished.py
+++ countyDemographicsUnfinished.py
@@ -19,29 +19,35 @@ def alphabetically_first_county(counties):
 
 def county_most_under_18(counties):
     """Return the name and state of a county ("<county name>, <state>") with the highest percent of under 18 year olds."""
-    first = counties[0]["Age"]["Percent Under 18 Years"]
     state = counties[0]["State"]
     for county in counties :
         if county["State"] < state:
             state = county["State"]
-        if county["Age"]["Percent Under 18 Years"] < first:
-            first = county["Age"]["Percent Under 18 Years"]
-    return first
+    return state
 
 
 def percent_most_under_18(counties):
     """Return the highest percent of under 18 year olds."""
-    first =
-    percent =
+    pmu18 = counties[0]["Age"]["Percent Under 18 Years"]
+    for county in counties:
+        if county["Age"]["Percent Under 18 Years"] < 18:
+            pmu18 = county["Age"]["Percent Under 18 Years"]
+    return pmu18
 
 def most_under_18(counties):
     """Return a list with the name and state of a county ("<county name>, <state>") and the percent of under 18 year olds for a county with the highest percent of under 18 year olds."""
-
+    state = counties[0]["Age"]["Percent Under 18 Years"]
+    for county in counties:
+        if county["Age"]["Percent Under 18 Years"] < 18:
+            state = county["Age"]["Percent Under 18 Years"]
+    return state
 
 def state_with_most_counties(counties):
     """Return a state that has the most counties."""
     #Make a dictionary that has a key for each state and the values keep track of the number of counties in each state
-
+    dict = {""}
+    if counties["State"] in dict:
+        
     #Find the state in the dictionary with the most counties
 
     #Return the state with the most counties
